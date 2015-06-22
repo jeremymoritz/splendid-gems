9769bc1b8292319971fbc351b042b1eb2d00f395
Moritz, David <Moritz.David@principal.com>
2015-06-22 16:55:04 -0500

    add

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,9 @@ mainApp.controller('MainCtrl', [
 	'$scope',
 	'$timeout',
 	'$interval',
+	'GemFactory',
 	'MethodFactory',
-	function MainCtrl($s, $timeout, $interval, MF) {
+	function MainCtrl($s, $timeout, $interval, GF, MF) {
 		'use strict';
 
 		function init() {
@@ -24,11 +25,12 @@ mainApp.controller('MainCtrl', [
 			});
 		}
 
-		function Card(gem, cost) {
+		function Card(gem, cost, points) {
 			_.extend(this, {
 				name: gem,
 				gem: gem,
-				cost: cost
+				cost: cost,
+				points: points || 0
 			});
 		}
 
@@ -43,7 +45,8 @@ mainApp.controller('MainCtrl', [
 				name: name,
 				chips: [],
 				cards: [],
-				goals: []
+				goals: [],
+				index: $s.allPlayers.length
 			});
 		}
 
@@ -53,8 +56,13 @@ mainApp.controller('MainCtrl', [
 		_.assign($s, {
 			time: moment().format(timeFormat),
 			allPlayers: [],
-			allGems: [],
-			gameStatus: 'pre-game'
+			allChips: [],
+			allGems: GF.allGems,
+			gameStatus: 'pre-game',
+			ff: {
+				newPlayerName: ''
+			},
+			currentSelection: []
 		});
 
 		$s.addNewPlayer = function addNewPlayer() {
@@ -69,6 +77,59 @@ mainApp.controller('MainCtrl', [
 
 		$s.startGame = function startGame() {
 			$s.gameStatus = 'game-started';
+		};
+
+		$s.quickStart = function quickStart() {
+			$s.ff.newPlayerName = 'Joey';
+			$s.addNewPlayer();
+			$s.ff.newPlayerName = 'Chandler';
+			$s.addNewPlayer();
+			$s.ff.newPlayerName = 'Ross';
+			$s.addNewPlayer();
+			$s.startGame();
+		};
+
+		$s.collectChips = function collectChips() {
+			var index = $s.currentPlayer.index + 1;
+			_.each($s.currentSelection, function eachChip(gem) {
+				$s.currentPlayer.chips.push(new Chip(gem));
+			});
+
+			if (index === $s.allPlayers.length) {
+				index = 0;
+			}
+			$s.currentPlayer = _.find($s.allPlayers, {index: index});
+			$s.currentSelection = [];
+		};
+
+		$s.clearSelection = function clearSelection() {
+			$s.currentSelection = [];
+		};
+
+		$s.addChip = function addChip(gem) {
+			$s.currentSelection.push(_.clone(gem));
+		};
+
+		$s.gemAvailable = function notAvailable(gem) {
+			switch (true) {
+				case ($s.currentSelection.indexOf('gold') !== -1):
+					// you have a gold
+					return false;
+				case ($s.currentSelection.length && $s.currentSelection[0] === $s.currentSelection[1]):
+					// you have two of the same gem
+					return false;
+				case ($s.currentSelection.length === 3):
+					// you have three gems
+					return false;
+				case ($s.currentSelection.indexOf(gem) !== -1 && $s.currentSelection.length === 2):
+					// you have two different gems, those two are not available
+					return false;
+				case ($s.currentSelection.length !== 0 && gem === 'gold'):
+					// you have a gem, gold is not available
+					return false;
+			}
+
+			return true;
 		};
 	}
 ]);
---
+++ src/js/services/GemFactory.js
@@ -0,0 +1,39 @@
+mainApp.factory('GemFactory', [
+	function GemFactory() {
+		'use strict';
+
+		return {
+			allGems: [{
+				name: 'emerald',
+				display: 'Emerald',
+				btnClass: 'success',
+				color: 'green'
+			}, {
+				name: 'topaz',
+				display: 'Topaz',
+				btnClass: 'warning',
+				color: 'orange'
+			}, {
+				name: 'ruby',
+				display: 'Ruby',
+				btnClass: 'danger',
+				color: 'red'
+			}, {
+				name: 'sapphire',
+				display: 'Sapphire',
+				btnClass: 'primary',
+				color: 'blue'
+			}, {
+				name: 'diamond',
+				display: 'Diamond',
+				btnClass: 'default',
+				color: 'white'
+			}, {
+				name: 'gold',
+				display: 'Gold',
+				btnClass: 'gold',
+				color: 'yellow'
+			}]
+		};
+	}
+]);
