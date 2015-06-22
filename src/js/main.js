f3977b3818bb419c9540df803d0bda8781bf4f3d
Moritz, David <Moritz.David@principal.com>
2015-06-22 15:06:11 -0500

    make

--- src/js/main.js
+++ src/js/main.js
@@ -17,11 +17,58 @@ mainApp.controller('MainCtrl', [
 			}, false);
 		}
 
+		function Chip(gem) {
+			_.extend(this, {
+				name: gem,
+				gem: gem
+			});
+		}
+
+		function Card(gem, cost) {
+			_.extend(this, {
+				name: gem,
+				gem: gem,
+				cost: cost
+			});
+		}
+
+		function Goal(cost) {
+			_.extend(this, {
+				cost: cost
+			});
+		}
+
+		function Player(name) {
+			_.extend(this, {
+				name: name,
+				chips: [],
+				cards: [],
+				goals: []
+			});
+		}
+
 		var timeFormat = 'YYYY-MM-DD HH:mm:ss';
 
 		//	initialize scoped variables
 		_.assign($s, {
-			time: moment().format(timeFormat)
+			time: moment().format(timeFormat),
+			allPlayers: [],
+			allGems: [],
+			gameStatus: 'pre-game'
 		});
+
+		$s.addNewPlayer = function addNewPlayer() {
+			$s.currentPlayer = new Player($s.ff.newPlayerName);
+			$s.allPlayers.push($s.currentPlayer);
+			$s.ff.newPlayerName = '';
+		};
+
+		$s.changeCurrentPlayer = function changeCurrentPlayer(player) {
+			$s.currentPlayer = player;
+		};
+
+		$s.startGame = function startGame() {
+			$s.gameStatus = 'game-started';
+		};
 	}
 ]);
